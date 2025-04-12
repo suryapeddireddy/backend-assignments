dbb1242bacc8c73eab5aead38533ddcd69267270
suryapeddireddy <suryapeddireddy1203@gmail.com>
2025-04-13 00:00:42 +0530

    Updates error message for tweet retrieval
    Modifies the error message returned when failing to get a tweet by ID to provide a more user-friendly and informative response.

--- controllers/tweet.controllers.js
+++ controllers/tweet.controllers.js
@@ -70,7 +70,7 @@ return res.status(404).json({message:"Tweet not found"});
 }
 return res.status(200).json({tweet});
 } catch (error) {
-return res.status(500).json({message:error.message});   
+return res.status(500).json({message:"falied to get tweet by id"});   
 }
 }
 
